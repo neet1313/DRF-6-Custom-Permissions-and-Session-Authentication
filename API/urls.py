4b6026ffca01bba1e95bfb693f78f32699528117
Prabhneet <prabhneet13@gmail.com>
2023-05-07 20:03:25 +0530

    Custom Permission and Session Authentication

--- API/urls.py
+++ API/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register('api', views.StudentViewset)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- API/views.py
+++ API/views.py
@@ -1,25 +1,18 @@
 from rest_framework import viewsets
 from API import serializers, models
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.authentication import SessionAuthentication
+from .custompermissions import CustomPermission
 # Create your views here.
 
 # ------- Model Serializer ------
 
 
 class StudentViewset(viewsets.ModelViewSet):
-    serializer_class = serializers.StudentSerializer
     queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
 
-    #  ------ Basic Authentication ------
-    authentication_classes = [BasicAuthentication]
-    # By default permission_classes = [AllowAny]
-    permission_classes = [IsAuthenticated]
+    #  ------ Session Authentication ------
 
-# AllowAny -> Anyone can access the API
-# IsAuthenticated -> User, Admin and Superuser can login
-# IsAdminUser -> Only Staff user(Admin) or Superuser can access the API
-# IsAuthenticatedOrReadOnly -> Authenticated user can change, Non-authenticated user can only read the API
-# DjangoModelPermissions -> We can control in admin panel that which user can user either of the GET, POST, PUT, PATCH, DELETE methods in the Viewset
-# DjangoModelPermissionsOrAnonReadOnly -> same as DjangoModelPermissions, but allows unauthenticated user to read-only the API
+    # Add Path in urls.py for session Authentication
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [CustomPermission]
